e356efc42e5ff22b60fca1ee2f0bcff4a141463e
nico-j3 <nicolejuarezflores@gmail.com>
2024-01-21 21:02:58 -0500

    removal

--- palette-picker/local-storage.js
+++ palette-picker/local-storage.js
@@ -1,3 +1,4 @@
+import initialPalettes from'./palettes.json'
 
 // Generic Helper Functions
 const setLocalStorageKey = (key, value) => {
@@ -7,11 +8,52 @@ const setLocalStorageKey = (key, value) => {
   const getLocalStorageKey = (key) => {
     try {
       return JSON.parse(localStorage.getItem(key))
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      console.error(error);
       return null;
     }
   }
+// sets the todos Array in localStorage with the key 'todos'
+
+export const setPalettes = (palettes) => setLocalStorageKey('palettes', palettes);
 
 
-  
+// returns the Array of all todo Objects from localStorage
+export const getAllPalettes = () => getLocalStorageKey('palettes');
+
+// adds a new todo Object to the Array of todos in localStorage
+export const addPalettes = (palettes) => setPalettes([...getAllPalettes(), palettes]);
+
+
+  // initializes todos using the todos.json file
+  export const initializePalettesIfEmpty = () => {
+    const existingPalettes = getAllPalettes();
+    if (!getAllPalettes()) {
+    setPalettes(initialPalettes)};
+
+  };
+
+
+// finds a todo by uuid and removes it from the Array of todos
+export const deletePalettes = (uuid) => {
+    const palettes = getAllPalettes().filter((palettes) => palettes.uuid !== uuid);
+    setPalettes(palettes);
+  };
+
+  // finds a todo by uuid and replaces it with the given todo in localStorage
+ export const togglePalettesComplete = (uuid) => {
+    const palettes = getAllPalettes();
+    const palettesToUpdate = palettes.find((palettes) => palettes.uuid === uuid);
+    palettesToUpdate.isComplete = !palettesToUpdate.isComplete
+    setPalettes(palettes);
+  };
+
+
+
+export const addAllPalettes = (palettes) => {
+    const existingPalettes = getAllPalettes();
+    console.log(existingPalettes, palettes)
+    // console.log(existingPalettes)
+    const newPalettesArray = [...existingPalettes, palettes];
+    setPalettes(newPalettesArray)
+}
--- palette-picker/main.js
+++ palette-picker/main.js
@@ -1,6 +1,11 @@
 import './style.css'
-import { v4 as uuidv4} from 'uuid';
 
+import initialPalettes from './palettes.json'
+
+import { v4 as uuidv4} from 'uuid';
+// data-store.js
+
+import {initializePalettesIfEmpty, getAllPalettes, addAllPalettes, deletePalettes} from './local-storage.js'
 
 
 //helper function
@@ -13,24 +18,74 @@ const handleSubmit = (e) => {
 
   newPalette.uuid = uuidv4();
 
+  addAllPalettes(newPalette);
 
-  // console.log('here is your data:', newPalette);
+  renderPalettes();
+
+  console.log('here is your data:', newPalette);
   // do something with formObj data...
+
+
 
   form.reset();
 }
 
+const renderPalettes = () => {
+
+// const paletteSections = document.querySelector('.palette-cards');
+// paletteSections.classList.add('palette-cards');
+
+// const fullPalettes = getAllPalettes()
+//   fullPalettes.forEach(section => {
+//     const card = document.createElement('div');
+//     card.id = 'submitted-colors'
+//     const paletteSections = document.querySelector('#submitted-colors');
+//     paletteSections.appendChild(card)
+  
+//   })
+
+
+  const defaultUl = document.querySelector('.chosen-colors');
+  defaultUl.innerHTML = ''
+  const localStoragePalettes = getAllPalettes()
+  
+  localStoragePalettes.forEach(item => {
+    const li = document.createElement('li')
+    li.classList.add('new-colors')
+    li.innerHTML = `
+    <h3 class="palette-title" > ${item.title}</h3>
+      <p> ${item.firstColor}</p>
+      <p> ${item.secondColor}</p>
+      <p> ${item.thirdColor}</p>
+    <p> ${item.temperature}</p>
+    `
+    defaultUl.append(li);
+
+  });
+  // console.log(localStoragePalettes);
+}
 
 
 
 
 //runner function
 const main = () => {
-const form = document.querySelector('#new-palette-form');
-form.addEventListener('submit', handleSubmit )
+  const form = document.querySelector('#new-palette-form');
+  form.addEventListener('submit', handleSubmit )
+  console.log(initialPalettes)
+  initializePalettesIfEmpty();
+  renderPalettes();
+
+  const removal = document.querySelector('.chosen-colors');
+  removal.addEventListener('click', (e) => {
+    e.target.remove()
+  }
+  )
 };
 
 main();
+
+
 
 
 
